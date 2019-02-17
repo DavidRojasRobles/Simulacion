810023d06383d28bc45510867faf37c41280a0f9
Marianne Rojas <mariannesolangel6a@gmail.com>
2019-02-17 14:25:10 -0500

    Tarea 3 sin GUI
    Simula RoundRobin para 2 procesos y un ascensor

---
+++ Tarea_3_colas_procesos.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+class Proceso:
+    IdProc = 0
+    def __init__(self, tiempo, estado):
+        Proceso.IdProc += 1
+        self.IdProc = Proceso.IdProc
+        self.tiempo = tiempo
+
+    def getIdProc(self):
+        return self.IdProc
+
+    def setTiempo(self, tiempo):
+        self.tiempo = tiempo
+
+    def getTiempo(self):
+        return self.tiempo
+
+    def ejecutar(self, q):
+        temp = self.tiempo
+        self.tiempo -= q
+        if self.tiempo < 0:
+            self.tiempo = 0
+            return temp
+        else:
+            return q
+
+class RoundRobin:
+    def __init__(self, quantum, numProc):
+        self.quantum = quantum
+        self.numProc = numProc
+        self.tiempoTotal = 0
+
+    def crearProceso(self):
+        p = Proceso(np.random.randint(1,100), True)
+        print "Proceso %d --> Tiempo = %d"%(p.getIdProc(), p.getTiempo())
+        return p
+
+    def crearCola(self):
+        self.cola = Cola()
+        for i in range(self.numProc):
+            self.cola.agregar(self.crearProceso())
+
+    def procesar(self):
+        self.crearCola()
+        print "\nProceso \t T. restante \t Total"
+        turnos = 0
+        while self.cola.estaVacia() == False:
+            turnos += 1
+            q = self.cola.getFirst().ejecutar(self.quantum)
+            self.tiempoTotal += q
+            print "%d \t\t %d \t\t %d"%(self.cola.getFirst().getIdProc(), self.cola.getFirst().getTiempo(), self.tiempoTotal)
+
+            if self.cola.getFirst().tiempo > 0:
+                self.cola.agregar(self.cola.getFirst())
+
+            self.cola.avanzar();
+
+        return turnos
+
+class Cola:
+    count = -1
+    def __init__(self):
+        self.items = []
+
+    def getFirst(self):
+        return self.items[0]
+
+
+    def getItems(self):
+        return self.items
+
+    def getItem(self, ix):
+        return self.items[ix]
+
+    def estaVacia(self):
+        if Cola.count < 0:
+            return True
+        else:
+            return False
+
+    def agregar(self, item):
+        Cola.count += 1
+        self.items.append(item)
+
+    def avanzar(self):
+        Cola.count -= 1
+        return self.items.pop(0)
+
+    def tamano(self):
+        return len(self.items)
+
+
+n = 3
+q = 10
+
+turnos = RoundRobin(q, n).procesar()
+print "Se completo en un total de %d turnos"%(turnos)
---
+++ Trea_3_ascensor.py
@@ -0,0 +1,59 @@
+import numpy as np
+from collections import OrderedDict as dict
+
+class Ascensor:
+
+    def __init__(self, pisos):
+        self.pasajeros = 0
+        self.piso = 0
+        self.maxPiso = pisos
+        self.limite = 10
+
+    def setPasajeros(self, pasajeros):
+        self.pasajeros = pasajeros
+
+    def getPasajeros(self):
+        return self.pasajeros
+
+    def setLimite(self, limite):
+        self.limite = limite
+
+    def getLimite(self):
+        return self.limite
+
+    # def setPiso(self, piso):
+    #     self.piso = piso
+
+    def getPiso(self):
+        return self.piso
+
+    def ingresan(self, ingresan):
+        self.pasajeros += ingresan
+
+    def salen(self, salen):
+        self.pasajeros -= salen
+
+    def subir(self):
+        if self.piso < self.maxPiso:
+            self.piso +=1
+
+    def bajar(self):
+        if self.piso > 0:
+            self.piso -=1
+
+pisos = 4
+viajes = 3
+ascensor = Ascensor(pisos)
+registro = {}
+for i in range(pisos*2*viajes):
+    salen = np.random.randint(0,ascensor.getPasajeros()) if ascensor.getPasajeros() > 0 else 0
+    ascensor.salen(salen)
+    ingresan = np.random.randint(0,(ascensor.getLimite()-ascensor.getPasajeros())) if ascensor.getPasajeros() < ascensor.getLimite() else 0
+    ascensor.ingresan(ingresan)
+    registro[ascensor.getPiso()] = salen, ingresan, ascensor.getPasajeros()
+
+    ascensor.subir() if i < pisos-1 else ascensor.bajar()
+    if i != 0 and (i+1)%pisos == 0:
+        # if (i+1)%pisos*2 == 0:
+        #     print i+1, dict(sorted(registro.items(), reverse = True))
+        print i+1, registro
